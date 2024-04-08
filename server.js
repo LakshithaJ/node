23580d0688364db5553924c29f0ccaa11c3c4dbd
LakshithaRJ <lakshrj139@gmail.com>
2024-04-08 12:16:10 -0500

    add a reponse html file

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
-    console.log('request');
+
+    // set the response headers
+    res.setHeader('Content-Type', 'text/html');
+
+    // send an html response
+    fs.readFile('./views/index.html', (err, data) =>{
+        if(err){
+            console.log('error', err);
+            res.end();
+        }else{
+            // res.write(data); // no need to write data like this if you have a single file, just sent it in the res.end() method
+            res.end(data);
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
