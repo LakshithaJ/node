a456000af6849337a78b908e2986fe2c6a292ada
LakshithaRJ <lakshrj139@gmail.com>
2024-04-11 18:08:52 -0500

    add mongoose to manage mongoDB connection, route methods

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { redirect } = require('next/dist/server/api-utils');
 
-// express app
+// connect to the MongoDB server and listen for requests
+const dbURI = 'mongodb+srv://test:Jo2ecCOZt2uOh9ce@cluster0.kpz7fs4.mongodb.net/';
+
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+  // express app
 const app = express();
-
-// listen for requests
-app.listen(3000);
-
-// middleware and static files
-app.use(express.static('public'))
-
-// use the morgan middleware
-app.use(morgan('dev'))
 
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
 
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-   // no need to specify any path, will directly look into the views folder and render the file mentioned
-  res.render('index', { title: 'Home', blogs });
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      // no need to specify any path, will directly look into the views folder and render the file mentioned
+      res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get('/about', (req, res) => {
@@ -33,6 +42,39 @@ app.get('/about', (req, res) => {
 
 app.get('/blogs/create', (req, res) => {
   res.render('create_blog', { title: 'Create a new blog' });
+});
+
+app.post('/new-blog', (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(result => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blog/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+  .then(result => {
+    res.render('details', {blog: result, title: 'Blog Details'});
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+app.delete('/blog/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 // 404 page
